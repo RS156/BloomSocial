ae14069aea7bb79d021a39c7f5cd1c5b8a662195
RS156 <rishabh.sarkar0696@gmail.com>
2023-03-25 20:26:21 +0530

    added body post and rightbar design components

--- bloom-social-app/src/components/rightbar/Rightbar.jsx
+++ bloom-social-app/src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
-import './rightbar.css'
+import React from "react";
+import "./rightbar.css";
 
 export default function Rightbar() {
   return (
-    <div className='rightbarContainer'>Rightbar</div>
-  )
+    <div className="rightbarContainer">
+      <div className="rightbarWrapper">
+        <div className="rightbarBirthdayContainer">
+          <img src="assets/gift.png" alt="" className="rightbarBirthayImg" />
+          <div className="rightbarBrthdayText">
+            <b>Barbara Everett</b> and <b>2 other friends</b> have birthday
+            today
+          </div>
+        </div>
+        <img src="/assets/ad.jpg" alt="" className="rightBarAdImg" />
+        <div className="rightbarFriendContainer">
+          <div className="rightbarFriendHeader">Online Friends</div>
+          <ul className="rightbarFriendList">
+            <li className="rightbarFriend">
+              <div className="rightbarFriendImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarFriendImage" />
+                <span className="rightbarFriendOnlineBadge"></span>
+              </div>
+              <div className="rightbarFriendname">Brenda Woods</div>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarFriendImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarFriendImage" />
+                <span className="rightbarFriendOnlineBadge"></span>
+              </div>
+              <div className="rightbarFriendname">Brenda Woods</div>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarFriendImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarFriendImage" />
+                <span className="rightbarFriendOnlineBadge"></span>
+              </div>
+              <div className="rightbarFriendname">Brenda Woods</div>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarFriendImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarFriendImage" />
+                <span className="rightbarFriendOnlineBadge"></span>
+              </div>
+              <div className="rightbarFriendname">Brenda Woods</div>
+            </li>
+            <li className="rightbarFriend">
+              <div className="rightbarFriendImgContainer">
+                <img src="assets/person/3.jpg" alt="" className="rightbarFriendImage" />
+                <span className="rightbarFriendOnlineBadge"></span>
+              </div>
+              <div className="rightbarFriendname">Brenda Woods</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
