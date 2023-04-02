ff917beff0fb7b03e5d6bbee830df9872371a31d
RS156 <rishabh.sarkar0696@gmail.com>
2023-04-02 20:36:35 +0530

    added router routes

---
+++ bloom-social-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Signup from "./pages/signup/Signup";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
